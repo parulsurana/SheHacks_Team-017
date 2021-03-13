d0fa79a7ec634c74216b1fa74e5ec9beec7e20e5
parulsurana <parulsurana28@gmail.com>
2021-03-13 19:22:30 +0530

    init

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Button.css';
+function Button({buttonName, label}) {
+  return (
+    <button
+      className={buttonName}>
+      {label}
+    </button>
+  );
+}
+export default Button;
---
+++ src/Components/features-card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Card.css"
+function Card({title,icon}) {
+    return (
+        <div className="card-container">
+        <div className="icon">
+           {icon}
+        </div>
+        <div className="card_title">
+            <h2>{title}</h2>
+        </div>
+    </div>
+    );
+}
+
+export default Card;
